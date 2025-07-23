afd6c5e230ec43ec9a6176d2ff8b17d59665d581
igarbayo <ignacio.garbayo@cht.forest.grupohotusa.int>
2025-07-23 13:24:25 +0200

    deployment

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,14 +4,23 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ) {};
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService,
+  ) {
+    this.defaultLimit = this.configService.get<number>('defaultLimit', 7);
+    //console.log(`Default limit set to: ${this.defaultLimit}`); // For debugging purposes
+  };
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -24,8 +33,15 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit=this.defaultLimit, offset=0 } = paginationDto;
+    return this.pokemonModel.find()
+      .limit(limit)
+      .skip(offset)
+      .sort({
+        no:1
+      })
+      .select('-__v');
   }
 
   async findOne(term: string): Promise<Pokemon> {
